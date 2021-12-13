3807d785e7e86426f8808a9fc138db1771b87469
suranga <56903228+surangajayalath@users.noreply.github.com>
2021-12-13 18:51:01 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body style="background-color: gray">
+<h1 style="color:white;text-align: center;">
+<?php echo "Hi, I am running on Docker....😆"?>
+</h1>
+</body>
+</html>
